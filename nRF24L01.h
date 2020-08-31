cb201239b22726aeca851bc71dedba50cbbf19bf
Przemysław Papla <przemekpapla@gmail.com>
2020-08-31 19:34:07 +0200

    nRF24L01 registers

---
+++ nRF24L01.h
@@ -0,0 +1,34 @@
+/*
+ * nRF24L01.h
+ */
+
+// Instructions
+#define R_REGISTER 0b00000000 // 1-5 LSB
+#define W_REGISTER 0b00100000 // 1-5 LSB
+#define R_RX_PAYLOAD 0b01100001
+#define W_TX_PAYLOAD 0b10100000
+#define FLUSH_TX 0b11100001
+#define FLUSH_RX 0b11100010
+#define REUSE_TX_PL 0b11100011
+#define NOP 0b11111111
+
+// Registers
+#define CONFIG 0x00
+#define EN_AA 0x01
+#define EN_RXADDR 0x02
+#define SETUP_AW 0x03
+#define SETUP_RETR 0x04
+#define RF_CH 0x05
+#define RF_SETUP 0x06
+#define STATUS 0x07
+#define OBSERVE_TX 0x08
+#define CD 0x09
+#define RX_ADDR_P0 0x0A
+#define RX_ADDR_P1 0x0B
+#define RX_ADDR_P2 0x0C
+#define RX_ADDR_P3 0x0D
+#define RX_ADDR_P4 0x0E
+#define RX_ADDR_P5 0x0F
+#define TX_ADDR 0x10
+#define RX_PW_P0 0x11
+#define RX_PW_P1 0x12
